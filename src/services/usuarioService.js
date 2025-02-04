7f7f31680a127affcc4e1ff6bf1dcbcb7986a601
chemagarciaff <josemaria.garciafernandez@riberadeltajo.es>
2025-02-04 16:07:41 +0100

    Creamos el servicio de usuario

---
+++ src/services/usuarioService.js
@@ -0,0 +1,57 @@
+const Usuario = require("../database/models/Usuario");
+
+const getAllUsers = async () => {
+  try {
+    return await Usuario.findAll();
+  } catch (error) {
+    throw new Error("Error al pedir todos los usuarios: " + error.message);
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    return await Usuario.findByPk(id);
+  } catch (error) {
+    throw new Error("Error al pedir un usuario por id: " + error.message);
+  }
+};
+
+const createUser = async (userData) => {
+  try {
+    return await Usuario.create(userData);
+  } catch (error) {
+    throw new Error("Error al crear el cliente: " + error.message);
+  }
+};
+
+const updateUser = async (id, userData) => {
+  try {
+    const usuario = await Usuario.findByPk(id);
+    if (!usuario) {
+      throw new Error("Usuario no encontrado");
+    }
+    return await usuario.update(userData);
+  } catch (error) {
+    throw new Error("Error al borrar modificar el usuario: " + error.message);
+  }
+};
+
+const deleteUser = async (id) => {
+  try {
+    const usuario = await Usuario.findByPk(id);
+    if (!usuario) {
+      throw new Error("El usuario no existe");
+    }
+    return await cliente.destroy();
+  } catch (error) {
+    throw new Error("Error al borrar el usuario: " + error.message);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
