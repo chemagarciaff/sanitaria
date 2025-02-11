0525eb5c678046b16d279fd7c16991a8e1fa231b
Sergio <sergio.ruedagonzalez@riberadeltajo.es>
2025-02-11 16:24:50 +0100

    resolucion de conflictos cambiarContrasenia

--- backEnd/src/controllers/usuarioController.js
+++ backEnd/src/controllers/usuarioController.js
@@ -58,7 +58,7 @@ const createUser = async (req, res) => {
     const createdUser = await usuarioService.createUser(userData);
 
     res.status(200).json(createdUser);
-  
+
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
